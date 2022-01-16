ad02b572db3086d18cda51a866b6683ae675a9b2
SFachinger <steffen.fachinger@online.de>
2022-01-16 09:12:23 +0100

    Add files via upload

--- src/GroupdataBundle.php
+++ src/GroupdataBundle.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
 /*
  * This file is part of TestBundle.
- * 
+ *
  * (c) Steffen Fachinger 2022 <steffen.fachnger@online.de>
  * @license GPL-3.0-or-later
  * For the full copyright and license information,
@@ -12,7 +12,7 @@ declare(strict_types=1);
  * @link https://github.com/sfachinger/contao-TestBundle
  */
 
-namespace sfachinger\GroupdataBundle;
+namespace SFachinger\GroupdataBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -29,7 +29,7 @@ class GroupdataBundle extends Bundle
 	public function build(ContainerBuilder $container): void
 	{
 		parent::build($container);
-		
+
 	}
-} 
+}
 ?>
